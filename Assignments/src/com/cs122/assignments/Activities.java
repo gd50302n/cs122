fb63c4bd6c311d21d3a0aa6bde81920333d69d94
Gabriel <Gabriel@gabriels-air>
2020-09-02 17:22:22 -0400

    09/02

---
+++ Assignments/src/com/cs122/assignments/Activities.java
@@ -0,0 +1,14 @@
+package com.cs122.assignments;
+
+public class Activities {
+	String activityname;
+	String locationType;
+	int suggestedHours;
+	
+	
+	public Activities
+	ActivityName = "Piano";
+	LocationType = "Indoor";
+	suggestedhours
+}
+	
