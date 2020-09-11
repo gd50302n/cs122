cade81527c9a943f3a67b03c0029a6d5578dc765
Gabriel <Gabriel@gabriels-air>
2020-09-10 21:17:59 -0400

    Lab for 09/10

---
+++ ClassLabs/src/com/cs122/classlabs/Checkings.java
@@ -0,0 +1,9 @@
+package com.cs122.classlabs;
+
+public class Checkings extends Deposit {
+	
+	public static void Checkings () {
+		return Checkings
+	}
+
+}
---
+++ ClassLabs/src/com/cs122/classlabs/Savings.java
@@ -0,0 +1,9 @@
+package com.cs122.classlabs;
+
+public class Savings extends Deposit {
+	
+	public static void limit() {
+		return limit;
+	}
+
+}
