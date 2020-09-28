ab7f8c8079d0dda2c7ec79f5b8f3e14cbe6e3913
Gabriel <Gabriel@gabriels-air>
2020-09-28 14:36:46 -0400

    class labs 09/28
    I tried doing the coding but it was giving me alot of errors.

---
+++ ClassLabs/src/com/cs122/classlabs/GuiProgramming.java
@@ -0,0 +1,36 @@
+package com.cs122.classlabs;
+
+import javafx.application.Application;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GuiProgramming extends Application {
+	
+	void start(Stage primaryStage)
+	
+	text text1= new text(200,400), "I am learning new gui");
+	text1.setfill(color.White);
+
+	
+	circle circle = new Circle(250,250,250);
+	Circle.setfill(Color.BLACK);
+	
+	rectangle rectangle = new rectangle (100,100,100);
+	rectangle.setfill(Color.Blue);
+	
+	Group root = new Group(circle,rectangle,text1);
+	scene.scene = new scene(root, 500, 500)
+	primarystage.set("Gui programming");
+	
+	static void main(String[]args)
+	
+	launch(args);
+	//not sure why some of the words are not colorful
+	
+
+}
