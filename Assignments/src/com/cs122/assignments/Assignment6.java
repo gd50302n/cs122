e3aabf757e7897944d689b9007f76f63f68e9595
Gabriel <Gabriel@gabriels-air>
2020-11-18 19:20:34 -0500

    Assignment 6 Homework

--- Assignments/src/com/cs122/assignments/Assignment6.java
+++ Assignments/src/com/cs122/assignments/Assignment6.java
@@ -1,5 +1,71 @@
 package com.cs122.assignments;
 
+import java.util.Scanner;
+
 public class Assignment6 {
+	
+	//problem 12.2
+	
+	//base case
+	if y=0(y) =1;
+	else power(y) = power(y-1) *x;
+	
+	//problem12.7
+	
+	Public string reverse(string s)
+	//i wouldnot use recursion becuase it is asking me to reverse it
+	
+	
+	
+	//problem12.1
+	
+public static void main(String[] args)
+//im havin trouble with this one
+
+
+//12.8
+public class Pascal {
+		public static void main(String[] args) {
+			Scanner scan= new Scanner(System.in);
+			System.out.print("Enter the number of rows to print: ");
+			int rows = scan.nextInt();
+			System.out.println("Pascal Triangle:");
+			print(rows);
+		}
+	
+	
+	public static void print(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k<n - i; k++) {
+				System.out.println(" ";);
+			}
+			for (int j = 0; j; j <=i; j++) {
+				System.out.print(pascal(i,j)+"");
+				}
+				System.out.print();
+		}
+	}
+	
+	public static int pascal (int i, int j) {
+		if(j == 0 {
+			return1;
+			else {
+				return pascal(i - 1) + pascal
+			}
+		}
+				
+	}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
 
 }
