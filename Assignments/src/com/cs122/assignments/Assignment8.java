e81696ffb436d63de01ad94d5da1e5e12726fb41
Gabriel <Gabriel@gabriels-air>
2020-12-03 20:02:12 -0500

    Assingmnet 8 due thursday.Had difficulty

---
+++ Assignments/src/com/cs122/assignments/Assignment8.java
@@ -0,0 +1,38 @@
+package com.cs122.assignments;
+
+public class Assignment8 {highIndex - the maximum index of the part of the list being searched
+
+lowIndex - the minimum index of the part of the list being searched
+
+target -- the item being searched for
+
+
+
+middleIndex = (highIndex + lowIndex) / 2
+
+if the list element at the middleIndex is the target
+
+return the middleIndex
+
+else
+
+if the list element in the middle is greater than the target
+
+binary search the first half of the list
+
+else
+
+binary search the second half of the list
+}
+
+public int binarysearch();
+
+public void print();
+{
+for ( int i = 0; i < list.length; i+ )
+ System.out.println( i + ":\t" + list[i] 
+		 
+for (int i = 0; 1);
+		// i have to find the smallest or lowest index. not sure how to code it. 
+
+
